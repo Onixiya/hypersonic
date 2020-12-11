086dc5ee80594feb5bc887a76ec77579e1e16abf
onixiya <nope@nope.com>
2020-12-11 16:50:16 +1100

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,60 @@
+﻿using MelonLoader;
+using Harmony;
+using NKHook6.Api;
+using NKHook6;
+using Assets.Scripts.Unity.UI_New.InGame;
+using NKHook6.Api.Events;
+using Assets.Scripts.Unity.UI_New.Popups;
+using Assets.Scripts.Unity.Bridge;
+using Assets.Scripts.Models.Towers.Behaviors;
+using Assets.Scripts.Simulation.Objects;
+using TMPro;
+
+namespace hypersonic
+{
+    public class Main : MelonMod
+    {
+        static float rate = 1;
+        static float timer = 0;
+        static System.Random random = new System.Random();
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            EventRegistry.instance.listen(typeof(Main));
+            Logger.Log("Hypersonic loaded");
+        }
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            bool inAGame = InGame.instance != null && InGame.instance.bridge != null;
+            if (inAGame)
+            {
+                timer += UnityEngine.Time.deltaTime;
+                if (rate != 1 && timer > 1)
+                {
+                    foreach (TowerToSimulation towerToSimulation in InGame.instance.bridge.GetAllTowers())
+                    {
+                        StartOfRoundRateBuffModel rateBuffSORModel = new StartOfRoundRateBuffModel("69", 1 / rate, 30);
+                        BehaviorMutator rateBuffModel = new StartOfRoundRateBuffModel.RateMutator(rateBuffSORModel);
+                        towerToSimulation.tower.AddMutator(rateBuffModel, 600, true, true, false, true, false, false);
+                    }
+                    timer = 0;
+                }
+            }
+        }
+        static Il2CppSystem.Action<float> rateDel = (Il2CppSystem.Action<float>)delegate (float s)
+        {
+            rate = s;
+        };
+        [EventAttribute("StartMatchEvent")]
+        public static void onEvent(NKHook6.Api.Events._InGame.InGameEvents.StartMatchEvent e)
+        {
+                Il2CppSystem.Action<string> mod = (Il2CppSystem.Action<string>)delegate (string s)
+                {
+                    rate = float.Parse(s);
+                };
+                PopupScreen.instance.ShowSetNamePopup("rate", "multiply fire rate by", mod, "0.33");
+                PopupScreen.instance.GetFirstActivePopup().GetComponentInChildren<TMP_InputField>().characterValidation = TMP_InputField.CharacterValidation.None;
+        }
+    }
+}
